6158ef984966ef953dac3a41a0705c2b0077a228
Ivan Kovalev <ikovalev.dev@gmail.com>
2022-05-16 15:27:23 +0300

    ART-COLOR(canvas)

---
+++ art-color/JavaScript/main.js
@@ -0,0 +1,5 @@
+/*Button */
+document.querySelector("#reset_layer")
+document.querySelector("#reset_all")
+document.querySelector("#search-color-product > input[type=text]")
+document.querySelector("#search-color-product")
